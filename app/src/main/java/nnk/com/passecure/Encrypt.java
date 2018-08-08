e30cbeeb0dcc26253bdb603c571fc352a4d6081d
Ejaz Hasnain <36451571+ejazhasnain@users.noreply.github.com>
2018-08-08 21:24:52 +0530

    Update Encrypt.java

--- app/src/main/java/nnk/com/passecure/Encrypt.java
+++ app/src/main/java/nnk/com/passecure/Encrypt.java
@@ -5,31 +5,40 @@ public class Encrypt {
      public static String one(String pass,String Upass)
      {
          String fi="";
+         int length=Upass.length();
+
+         String first=Upass.substring(0,length/2);
+         String last=Upass.substring(length/2,length);
+         StringBuilder str=new StringBuilder(first);
+         StringBuilder str1=new StringBuilder(last);
+         str.reverse();
+         str1.reverse();
+         str.append(str1);
+         str.reverse();
+         Upass=String.valueOf(str);
          char[] ch=Upass.toCharArray();
          for(int i=0;i<ch.length;i++)
          {
-             int num=ch[i];
-             if(num>=15 && num<=127)
+             int val=ch[i];
+             if(val>=15 && val<=127)
              {
-                 num=num-1;
+                 val=val-1;
              }
-             System.out.println(num);
-             ch[i]=(char)num;
+
+             ch[i]=(char)val;
          }
 
          Upass=String.valueOf(ch);
-         StringBuffer str=new StringBuffer(Upass);
+         int Min=100;
+         int Max=999;
+         String val1=String.valueOf((Min + (int)(Math.random() * ((Max - Min)))));
+         Upass=val1+Upass;
+         String val3=String.valueOf((Min + (int)(Math.random() * ((Max - Min)))));
+         StringBuilder str2=new StringBuilder(Upass);
 
-         char[] ch1=pass.toCharArray();
-         for(int j=0;j<ch1.length;j=j+2)
-         {
-             fi=fi+String.valueOf(ch1[j]);
-         }
-         System.out.println(fi);
-         str.append(fi);
-         str.append("659");
-         str.reverse();
-         Upass=str.toString();
+         str2.append(val3);
+         str2.reverse();
+         Upass=String.valueOf(str2);
          return Upass;
      }
 
