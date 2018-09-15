91e31cef4fb340898854af08587ad0fa6d403885
Ejaz Hasnain <36451571+ejazhasnain@users.noreply.github.com>
2018-09-15 21:05:01 +0530

    Update Encrypt.java

--- app/src/main/java/nnk/com/passecure/Encrypt.java
+++ app/src/main/java/nnk/com/passecure/Encrypt.java
@@ -1,45 +1,24 @@
 package nnk.com.passecure;
 
+
+import com.scottyab.aescrypt.AESCrypt;
+
+import java.security.GeneralSecurityException;
+
 public class Encrypt {
 
-     public static String one(String pass,String Upass)
-     {
-         String fi="";
-         int length=Upass.length();
+    static String encryptedMsg;
+    public static String one(String pass, String Upass) {
 
-         String first=Upass.substring(0,length/2);
-         String last=Upass.substring(length/2,length);
-         StringBuilder str=new StringBuilder(first);
-         StringBuilder str1=new StringBuilder(last);
-         str.reverse();
-         str1.reverse();
-         str.append(str1);
-         str.reverse();
-         Upass=String.valueOf(str);
-         char[] ch=Upass.toCharArray();
-         for(int i=0;i<ch.length;i++)
-         {
-             int val=ch[i];
-             if(val>=15 && val<=127)
-             {
-                 val=val-1;
-             }
+        String password = pass;
+        String message = Upass;
+        try {
+            encryptedMsg = AESCrypt.encrypt(password, message);
+        }catch (GeneralSecurityException e){
+            //handle error
+        }
 
-             ch[i]=(char)val;
-         }
-
-         Upass=String.valueOf(ch);
-         int Min=100;
-         int Max=999;
-         String val1=String.valueOf((Min + (int)(Math.random() * ((Max - Min)))));
-         Upass=val1+Upass;
-         String val3=String.valueOf((Min + (int)(Math.random() * ((Max - Min)))));
-         StringBuilder str2=new StringBuilder(Upass);
-
-         str2.append(val3);
-         str2.reverse();
-         Upass=String.valueOf(str2);
-         return Upass;
-     }
-
+        return encryptedMsg;
+        
+    }
 }
